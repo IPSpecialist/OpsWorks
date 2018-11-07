551e4bad90ea7d732c06e6439d75f8abb98b8994
IPSpecialist <43603376+IPSpecialist@users.noreply.github.com>
2018-11-07 11:47:37 +0500

    Add files via upload

---
+++ IPOpsWork.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Welcome to IPSpecialist</title>
+    </head>
+
+    <body>
+<div align="center">
+                <h1>IPSpecialist</h1>
+                <h2>Congratulations! On successfully installing an application to OpsWorks!</h2>
+                <div align="center"><img src="IP.png"></div>
+                <p>Your Application is now running on the host "<?php echo gethostname(); ?>"; in a new stack and layer in OpsWorks.</p>
+                <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
+        </div>
+    </body>
+
+</html>
